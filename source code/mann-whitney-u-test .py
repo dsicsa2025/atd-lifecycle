c21320e785e8422ed381a035d76d3abcc1d8ac2d
dsicsa2025 <71711581+dsicsa2025@users.noreply.github.com>
2024-11-19 10:43:36 +0100

    Update mann-whitney-u-test .py

--- source code/mann-whitney-u-test .py
+++ source code/mann-whitney-u-test .py
@@ -11,20 +11,20 @@ data = pd.read_csv(file_path)
 data['intro-FAN-IN'] = pd.to_numeric(data['intro-FAN-IN'], errors='coerce')
 data['payment-FAN-IN'] = pd.to_numeric(data['payment-FAN-IN'], errors='coerce')
 
-
-
-#FAN-IN
-before = data['intro-FAN-IN'].dropna(axis=0, how='any')
-after = data['payment-FAN-IN'].dropna(axis=0, how='any')
-
+data['intro-FAN-OUT'] = pd.to_numeric(data['intro-FAN-OUT'], errors='coerce')
+data['payment-FAN-OUT'] = pd.to_numeric(data['payment-FAN-OUT'], errors='coerce')
 
 #FAN-IN
-# before = intro_data['INTRO-Calls-Dependencies']
-# after = payment_data['PAYMENTNACallsNADependencies']
+before_in = data['intro-FAN-IN'].dropna(axis=0, how='any')
+after_in = data['payment-FAN-IN'].dropna(axis=0, how='any')
 
+#FAN-OUT
+before_out = data['intro-FAN-OUT'].dropna(axis=0, how='any')
+after_out = data['payment-FAN-OUT'].dropna(axis=0, how='any')
 
+# Mann-Whitney U Test 
+u_stat, p_value = mannwhitneyu(before_in, after_in)
+print(f'FAN-IN ATD - Mann-Whitney U Statistic: {u_stat}, p-value: {p_value}')
 
-
-# Mann-Whitney U Test untuk dua grup tidak berpasangan
-u_stat, p_value = mannwhitneyu(before, after)
-print(f'FAN-IN NON-ATD - Mann-Whitney U Statistic: {u_stat}, p-value: {p_value}')
+u_stat, p_value = mannwhitneyu(before_out, after_out)
+print(f'FAN-OUT ATD - Mann-Whitney U Statistic: {u_stat}, p-value: {p_value}')
