f87be64d4dfd7e30f720a6b9fff8237c1f7ebf41
dsicsa2025 <71711581+dsicsa2025@users.noreply.github.com>
2024-11-19 10:36:51 +0100

    Update partial-spearmans-correlation.py

--- source code/partial-spearmans-correlation.py
+++ source code/partial-spearmans-correlation.py
@@ -6,7 +6,7 @@ data = pd.read_csv("ANALYZE-RESULTS/atd_final.csv")
 
 data = data.dropna(subset=['number_of_changes', 'payment-complexity', 'payment-sloc'])
 
-# We want to control the variable 'LOC' (Lines of Code)
+# We want to control the variable 'SLOC' (Source Lines of Code)
 
 # Calculate Partial Spearman Correlation
 partial_corr = pg.partial_corr(data=data,
